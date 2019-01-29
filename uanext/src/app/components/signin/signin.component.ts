94ab6f2146e475e8f52e486e5bd308111fd28c7b
Igor Omelyuskho <omelyushko.igor@gmail.com>
2019-01-29 17:44:58 +0200

    edit signin component

--- uanext/src/app/components/signin/signin.component.ts
+++ uanext/src/app/components/signin/signin.component.ts
@@ -1,4 +1,6 @@
+import { AuthorizationService } from './../../services/authorization.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  signinForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthorizationService) {
+    this.signinForm = this.formBuilder.group({
+      login: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  get formControls() {
+    return this.signinForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.signinForm.invalid) {
+      return;
+    }
+
+    this.authService.signIn(this.signinForm.value).subscribe(
+      response => {
+        console.log(response);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 }
