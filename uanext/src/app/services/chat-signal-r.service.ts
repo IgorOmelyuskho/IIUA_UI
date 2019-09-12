5ef1ff1053aca49e700764a9c02ddbe6367d0045
omelyushko.igor <omelyushko.igor@gmail.com>
2019-09-12 10:39:46 +0300

    fix

--- uanext/src/app/services/chat-signal-r.service.ts
+++ uanext/src/app/services/chat-signal-r.service.ts
@@ -21,7 +21,7 @@ export class ChatSignalRService {
     // this.emulateSignalR();
     const token = localStorage.getItem('token');
 
-    const options = { accessTokenFactory: () => token, transport: HttpTransportType.None };
+    const options = { accessTokenFactory: () => token, transport: HttpTransportType.WebSockets };
 
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(environment.signalR, options).build();
