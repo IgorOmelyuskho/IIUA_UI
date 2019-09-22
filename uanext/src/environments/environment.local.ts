4c2c8b32b8ec51a36a09372492ceb2e8cfd68856
duxa <duxa321@gmail.com>
2019-09-23 00:13:17 +0300

    Fix signalR

--- uanext/src/environments/environment.local.ts
+++ uanext/src/environments/environment.local.ts
@@ -1,12 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-const domain = 'http://localhost:8080';
+const domain = 'http://localhost';
 
 export const environment = {
-  production: false,
-  signalR: domain + '/notifications/cord',
+  production: true,
+  signalR: domain + '/notifications/api/CoordinatesHub',
   auth:  domain + '/auth',
   projects: domain +  '/projects',
   map:  domain + '/map',
@@ -37,8 +33,6 @@ export const environment = {
   filteringProjects: '/api/FilteringProjects/Filtering-projects',
   sphereActivity: '/api/SphereActivity',
   changeQueuePosition: '/api/Projects/ChangeQueuePosition',
-  addProjectGeoObject: '/api/Projects/AddProjectGeoObject',
-  getLatestProjects: '/api/Projects/GetLatestProjects',
 
   get3DObject: '/api/Map/Get3DObject',
   post3DObject: '/api/Map/Post3DObject',
@@ -48,7 +42,6 @@ export const environment = {
   mapFilteringProjects: '/api/Map/projects-filtering',
   addGeoModel: '/api/Map/addGeoModel',
   getGeoModels: '/api/Map/getGeoModels',
-  updateGeoObjectSettings: '/api/Map/UpdateGeoObjectSettings',
 
   Notification: '/api/Notification',
 
@@ -62,11 +55,3 @@ export const environment = {
   getParticipantById: '/api/Participants/GetByParticipantId',
   getParticipantByChatId: '/api/Participants/GetByConversationId'
 };
-
-/*
- * In development mode, for easier debugging, you can ignore zone related error
- * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
- * because it will have a performance impact when errors are thrown
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
